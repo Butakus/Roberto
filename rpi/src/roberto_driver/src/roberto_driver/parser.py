a638490a51bce76dffffdf20f931b28e2c96001f
Butakus <butakus@gmail.com>
2017-04-01 13:47:31 +0200

    Finish ArdPiComm python parser

--- rpi/src/roberto_driver/src/roberto_driver/parser.py
+++ rpi/src/roberto_driver/src/roberto_driver/parser.py
@@ -15,11 +15,18 @@ def parse_str(data):
     return ''.join(chr(x) for x in data)
 
 
+def parse_int8(data, big_endian=False):
+    """ Convert 1 byte into a 8 bit signed int """
+    data_str = parse_str(data)
+    byte_format = '>b' if big_endian else '<b'
+    return struct.unpack(byte_format, data_str)[0]
+
+
 def parse_int16(data, big_endian=False):
     """ Convert 2 bytes into a 16 bit int """
-    str_data = parse_str(data)
+    data_str = parse_str(data)
     byte_format = '>h' if big_endian else '<h'
-    return struct.unpack(byte_format, str_data)[0]
+    return struct.unpack(byte_format, data_str)[0]
 
 
 def parse_uint16(data, big_endian=False):
@@ -34,9 +41,9 @@ def parse_uint16(data, big_endian=False):
 
 def parse_int32(data, big_endian=False):
     """ Convert 4 bytes into a 32 bit int """
-    str_data = parse_str(data)
+    data_str = parse_str(data)
     byte_format = '>i' if big_endian else '<i'
-    return struct.unpack(byte_format, str_data)[0]
+    return struct.unpack(byte_format, data_str)[0]
 
 
 def parse_uint32(data, big_endian=False):
@@ -49,36 +56,11 @@ def parse_uint32(data, big_endian=False):
     return val
 
 
-def parse_int64(data, big_endian=False):
-    # TODO: Convert 8 bytes into a 64 bit int (long long)
-    str_data = parse_str(data)
-    byte_format = '>q' if big_endian else '<q'
-    return struct.unpack(byte_format, str_data)[0]
-
-
-def parse_uint64(data, big_endian=False):
-    """ Convert 8 bytes into an unsigned 64 bit int (unsigned long long) """
-    val = 0
-    if big_endian:
-        val = data[7] | (data[6] << 8) | (data[5] << 16) | (data[4] << 24) | (data[3] << 32) | (data[2] << 40) | (data[1] << 48) | (data[0] << 56)
-    else:
-        val = data[0] | (data[1] << 8) | (data[2] << 16) | (data[3] << 24) | (data[4] << 32) | (data[5] << 40) | (data[6] << 48) | (data[7] << 56)
-    return val
-
-
-def parse_float32(data, big_endian=False):
+def parse_float(data, big_endian=False):
     """ Convert 4 bytes into a 32 bit float """
-    str_data = parse_str(data)
+    data_str = parse_str(data)
     byte_format = '>f' if big_endian else '<f'
-    return struct.unpack(byte_format, str_data)[0]
-    
-
-def parse_float64(data, big_endian=False):
-    """ Convert 8 bytes into a 64 bit float (double) """
-    str_data = parse_str(data)
-    byte_format = '>d' if big_endian else '<d'
-    return struct.unpack(byte_format, str_data)[0]
-
+    return struct.unpack(byte_format, data_str)[0]
 
 
 
@@ -94,41 +76,57 @@ def serialize_str(data):
     return [ord(x) for x in data]
 
 
+def parse_int8(data, big_endian=False):
+    """ Convert a 8 bit signed int into a byte """
+    byte_format = '>b' if big_endian else '<b'
+    data_str struct.pack(byte_format, data)
+    return serialize_str(data_str)
+
+
 def serialize_int16(data, big_endian=False):
-    # TODO: Convert a 16 bit int into 2 bytes
-    pass
+    """ Convert a 16 bit int into 2 bytes """
+    byte_format = '>h' if big_endian else '<h'
+    data_str struct.pack(byte_format, data)
+    return serialize_str(data_str)
 
 
 def serialize_uint16(data, big_endian=False):
-    # TODO: Convert an unsigned 16 bit int into 2 bytes
-    pass
+    """ Convert an unsigned 16 bit int into 2 bytes """
+    data_bytes = []
+    if big_endian:
+        data_bytes[1] = data & 0xFF
+        data_bytes[0] = (data >> 8) & 0xFF
+    else:
+        data_bytes[0] = data & 0xFF
+        data_bytes[1] = (data >> 8) & 0xFF
+    return data_bytes
 
 
 def serialize_int32(data, big_endian=False):
-    # TODO: Convert a 32 bit int into 4 bytes
-    pass
+    """ Convert a 32 bit int into 4 bytes """
+    byte_format = '>i' if big_endian else '<i'
+    data_str struct.pack(byte_format, data)
+    return serialize_str(data_str)
 
 
 def serialize_uint32(data, big_endian=False):
-    # TODO: Convert an unsigned 32 bit int into 4 bytes
-    pass
+    """ Convert an unsigned 32 bit int into 4 bytes """
+    data_bytes = []
+    if big_endian:
+        data_bytes[3] = data & 0xFF
+        data_bytes[2] = (data >> 8) & 0xFF
+        data_bytes[1] = (data >> 16) & 0xFF
+        data_bytes[0] = (data >> 24) & 0xFF
+    else:
+        data_bytes[0] = data & 0xFF
+        data_bytes[1] = (data >> 8) & 0xFF
+        data_bytes[2] = (data >> 16) & 0xFF
+        data_bytes[3] = (data >> 24) & 0xFF
+    return data_bytes
 
 
-def serialize_int64(data, big_endian=False):
-    # TODO: Convert a 32 bit int (long) into 8 bytes
-    pass
-
-
-def serialize_uint64(data, big_endian=False):
-    # TODO: Convert an unsigned 32 bit int (unsigned long) into 8 bytes
-    pass
-
-
-def serialize_float32(data, big_endian=False):
-    # TODO: a 32 bit float into 4 bytes
-    pass
-
-
-def serialize_float64(data, big_endian=False):
-    # TODO: a 64 bit float (double) into 8 bytes
-    pass
+def serialize_float(data, big_endian=False):
+    """ Convert a 32 bit float into 4 bytes """
+    byte_format = '>f' if big_endian else '<f'
+    data_str = struct.pack(byte_format, data)
+    return serialize_str(data_str)
